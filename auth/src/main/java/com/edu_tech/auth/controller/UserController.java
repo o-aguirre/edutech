e436a085a5f91ce83efc90f2d2a2dd4c232ea3e5
o-aguirre <two.aguirre@gmail.com>
2025-05-13 17:48:50 -0400

    ADD: security module

--- auth/src/main/java/com/edu_tech/auth/controller/UserController.java
+++ auth/src/main/java/com/edu_tech/auth/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.edu_tech.auth.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
 public class UserController {
+
+    @GetMapping
+    public String test() {
+        return "Test";
+    }
 }
